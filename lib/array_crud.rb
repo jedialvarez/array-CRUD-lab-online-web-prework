58ede8d37b9939971fbe36f3d4c65ecaa7c5a9ac
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-13 19:34:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  favorite_cars << "Toyota"
+  favorite_cars.push("Toyota")
 end
 
 def add_element_to_start_of_array(array, element)
